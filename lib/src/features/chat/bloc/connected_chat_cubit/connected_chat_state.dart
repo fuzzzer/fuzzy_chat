05d2909951e511f5da3f108dfb9ee068ebc18b2c
LukaLoriaNexus <luka.loria@nexus.ge>
2024-10-11 00:35:30 +0200

    feat: add connected chat cubit, chat actions and etc

---
+++ lib/src/features/chat/bloc/connected_chat_cubit/connected_chat_state.dart
@@ -0,0 +1,46 @@
+part of 'connected_chat_cubit.dart';
+
+class ConnectedChatState {
+  final StateStatus status;
+  final List<MessageData> messages;
+  final DefaultFailure? failure;
+
+  final StateStatus actionStatus;
+  final ChatActionType actionType;
+  final DefaultFailure? actionFailure;
+
+  const ConnectedChatState({
+    required this.status,
+    required this.messages,
+    this.failure,
+    this.actionStatus = StateStatus.initial,
+    this.actionType = ChatActionType.none,
+    this.actionFailure,
+  });
+
+  ConnectedChatState copyWith({
+    StateStatus? status,
+    List<MessageData>? messages,
+    DefaultFailure? failure,
+    StateStatus? actionStatus,
+    ChatActionType? actionType,
+    DefaultFailure? actionFailure,
+  }) {
+    return ConnectedChatState(
+      status: status ?? this.status,
+      messages: messages ?? this.messages,
+      failure: failure ?? this.failure,
+      actionStatus: actionStatus ?? this.actionStatus,
+      actionType: actionType ?? this.actionType,
+      actionFailure: actionFailure ?? this.actionFailure,
+    );
+  }
+}
+
+enum ChatActionType {
+  none,
+  sendMessage,
+  receiveMessage,
+  deleteMessage,
+  deleteAllMessages,
+}
