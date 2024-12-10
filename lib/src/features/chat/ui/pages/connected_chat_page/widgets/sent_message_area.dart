5cd84cebeaaab8e729c8f4071764b2f5fdf23759
LukaLoriaNexus <luka.loria@nexus.ge>
2024-12-10 03:24:10 +0100

    feat: apply normal colors

--- lib/src/features/chat/ui/pages/connected_chat_page/widgets/sent_message_area.dart
+++ lib/src/features/chat/ui/pages/connected_chat_page/widgets/sent_message_area.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fuzzy_chat/lib.dart';
 
-final _copyDebouncer = Debouncer(milliseconds: 500);
-
-class SentMessageArea extends StatelessWidget {
+class SentMessageArea extends StatefulWidget {
   final MessageData message;
 
   const SentMessageArea({
@@ -12,24 +10,43 @@ class SentMessageArea extends StatelessWidget {
     super.key,
   });
 
-  static void _copyMessage({
+  @override
+  State<SentMessageArea> createState() => _SentMessageAreaState();
+}
+
+class _SentMessageAreaState extends State<SentMessageArea> {
+  bool hasJustCopeied = false;
+
+  void _copyMessage({
     required String encryptedMessage,
     required FuzzyChatLocalizations localizations,
   }) {
-    _copyDebouncer.run(() {
-      Clipboard.setData(
-        ClipboardData(
-          text: encryptedMessage,
-        ),
-      ).then((value) {
-        scaffoldMessengerKey.currentState?.showSnackBar(
-          SnackBar(
-            content: Text(
-              localizations.copiedToTheClipboard,
-            ),
+    if (hasJustCopeied) return;
+
+    setState(() {
+      hasJustCopeied = true;
+    });
+
+    Clipboard.setData(
+      ClipboardData(
+        text: '$fuzzIdentificator$encryptedMessage',
+      ),
+    ).then((value) {
+      scaffoldMessengerKey.currentState?.showSnackBar(
+        SnackBar(
+          content: Text(
+            localizations.copiedToTheClipboard,
           ),
-        );
-      });
+        ),
+      );
+    });
+
+    Future.delayed(const Duration(seconds: 1), () {
+      if (mounted) {
+        setState(() {
+          hasJustCopeied = false;
+        });
+      }
     });
   }
 
@@ -47,32 +64,38 @@ class SentMessageArea extends StatelessWidget {
       bottomLeft: Radius.circular(12),
     );
 
-    return InkWell(
-      borderRadius: borderRadius,
-      onTap: () {
-        _copyMessage(
-          encryptedMessage: message.encryptedMessage,
-          localizations: localizations,
-        );
-      },
-      child: Container(
-        width: double.maxFinite,
-        margin: const EdgeInsets.symmetric(vertical: 4),
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Container(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.75,
-            ),
-            padding: const EdgeInsets.all(12),
+    return Container(
+      width: double.maxFinite,
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Material(
+          color: Colors.transparent,
+          child: Ink(
             decoration: BoxDecoration(
-              color: uiColors.secondaryColor,
               borderRadius: borderRadius,
+              color: uiColors.secondaryColor,
             ),
-            child: Text(
-              message.encryptedMessage,
-              style: uiTextStyles.body16.copyWith(
-                color: Colors.white,
+            child: InkWell(
+              borderRadius: borderRadius,
+              splashColor: uiColors.backgroundPrimaryColor,
+              onLongPress: () {
+                _copyMessage(
+                  encryptedMessage: widget.message.encryptedMessage,
+                  localizations: localizations,
+                );
+              },
+              child: Container(
+                constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.75,
+                ),
+                padding: const EdgeInsets.all(12),
+                child: Text(
+                  widget.message.encryptedMessage,
+                  style: uiTextStyles.body16.copyWith(
+                    color: uiColors.backgroundPrimaryColor,
+                  ),
+                ),
               ),
             ),
           ),
