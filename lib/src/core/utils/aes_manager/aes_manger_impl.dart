daa0c76ef4d8f8bd765f79a4dd0bdaf309990dcf
LukaLoriaNexus <luka.loria@nexus.ge>
2024-12-12 17:50:34 +0100

    feat: make messages more secure by encrypting them with derived ephemeral key

--- lib/src/core/utils/aes_manager/aes_manger_impl.dart
+++ lib/src/core/utils/aes_manager/aes_manger_impl.dart
@@ -12,10 +12,18 @@ class _AESManagerImpl {
 
   static Uint8List syncEncrypt(Uint8List inputBytes, Uint8List key) {
     final nonce = generateRandomSecureBytes(_nonceByteLength);
+
+    final ephemeralKey = _deriveEphemeralKey(mainKey: key, nonce: nonce);
+
     final encryptCipher = GCMBlockCipher(AESEngine())
       ..init(
         true,
-        AEADParameters(KeyParameter(key), macSize, nonce, Uint8List(0)),
+        AEADParameters(
+          KeyParameter(ephemeralKey),
+          macSize,
+          nonce,
+          Uint8List(0),
+        ),
       );
 
     final encryptedData = encryptCipher.process(inputBytes);
@@ -28,13 +36,39 @@ class _AESManagerImpl {
     final nonce = encryptedInputBytes.sublist(0, _nonceByteLength);
     final encryptedData = encryptedInputBytes.sublist(_nonceByteLength);
 
+    final ephemeralKey = _deriveEphemeralKey(mainKey: key, nonce: nonce);
+
     final decryptCipher = GCMBlockCipher(AESEngine())
       ..init(
         false,
-        AEADParameters(KeyParameter(key), macSize, nonce, Uint8List(0)),
+        AEADParameters(
+          KeyParameter(ephemeralKey),
+          macSize,
+          nonce,
+          Uint8List(0),
+        ),
       );
 
     final decryptedData = decryptCipher.process(encryptedData);
     return decryptedData;
+  }
+
+  static Uint8List _deriveEphemeralKey({
+    required Uint8List mainKey,
+    required Uint8List nonce,
+  }) {
+    final hkdf = HKDFKeyDerivator(SHA256Digest())
+      ..init(
+        HkdfParameters(
+          mainKey,
+          _keyByteLength,
+          null,
+          nonce,
+        ),
+      );
+
+    final derived = Uint8List(_keyByteLength);
+    hkdf.deriveKey(null, 0, derived, 0);
+    return derived;
   }
 }
