500be5b6d6eae73273c0dfe400d4eb0b182bec8b
fuzzzer <lukaloria11@gmail.com>
2025-04-18 19:21:20 +0400

    fix: info format

--- lib/src/core/constants/default_constants.dart
+++ lib/src/core/constants/default_constants.dart
@@ -1,4 +1,19 @@
+import 'dart:typed_data';
+
 const fuzzIdentificator = 'Fuzz/';
 const fuzzedFileIdentificator = 'fuzz';
 
-const fuzzVersionInfo = 'fuzz_v1';
+final fuzzVersionInfo = Uint8List.fromList(<int>[
+  90,
+  110,
+  86,
+  54,
+  101,
+  108,
+  57,
+  50,
+  77,
+  81,
+  61,
+  61,
+]);
