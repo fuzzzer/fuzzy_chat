7bb6b6d1ad45c4ba95d89f21f031634435d5585d
fuzzzer <lukaloria11@gmail.com>
2025-05-19 03:19:40 +0400

    chore: remove unused import

--- lib/src/core/utils/aes_manager/aes_manager.dart
+++ lib/src/core/utils/aes_manager/aes_manager.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 import 'dart:isolate';
-import 'dart:typed_data';
 import 'package:flutter/foundation.dart';
 import 'package:fuzzy_chat/lib.dart';
 import 'package:pointycastle/export.dart';
